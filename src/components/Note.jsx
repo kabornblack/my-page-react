f178f871c60b9797a9953b15100ab781dda6ce15
kabornblack <shaibu.kabiru@email.com>
2023-08-19 04:09:30 +0300

    initial commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+    return (
+        <note>
+            <h1>
+                Tittle
+            </h1>
+            <p>
+                Content
+            </p>
+        </note>
+    );
+}
+
+export default Note;
